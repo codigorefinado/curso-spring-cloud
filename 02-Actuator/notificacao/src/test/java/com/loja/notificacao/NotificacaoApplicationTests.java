16a2f82044bbf2b798cdeb0b0a93001703b56deb
Clayton Passos <netstart@gmail.com>
2019-08-19 23:27:02 -0300

    add actuator

---
+++ 02-Actuator/notificacao/src/test/java/com/loja/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.loja.notificacao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotificacaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
